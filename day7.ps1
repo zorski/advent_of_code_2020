6577f67733d79c755b3de612a4605727012edf6b
Michal Zaorski <m.zaorski@icloud.com>
2020-12-17 17:21:49 +0100

    part 2 of day7 - kinda :poop:

--- day7.ps1
+++ day7.ps1
@@ -13,22 +13,29 @@ function Parse-BagRules {
         $Parent, $Children = ($BagRule -split "contain").Trim()
         $Parent = ($Parent.Split(" ")[0..1] -join " ")
         if($Children -eq "no other bags.") {
-            $Children = @()
+            $Children = $null
         } else {
+            $ChildrenSet = @{}
             $Children = $Children.Split(",").Trim()
-            $Children = $Children -replace "^\d+\s",""
-            $Children = $Children -replace "bags?",""
-            $Children = $Children -replace "\.$",""
-            $Children = $Children.Trim()
-        }
-        $Children | ForEach-Object {
-            if($BagRulesParsedReversed.ContainsKey($PSItem)){
-                $BagRulesParsedReversed[$PSItem] += $Parent
-            } else {
-                $BagRulesParsedReversed.Add($PSItem,@($Parent))
-            }    
+            $Children | ForEach-Object {
+                $C = $PSItem.Split(" ")
+                $Quantity = [int]$C[0]
+                $Name = $C[1..2] -join " "
+                $ChildrenSet.Add($Name,$Quantity)
+            }
+            $Children = $ChildrenSet
         }
 
+        if ($Children) {
+            $Children.Keys | ForEach-Object {
+                if($BagRulesParsedReversed.ContainsKey($PSItem)){
+                    $BagRulesParsedReversed[$PSItem] += $Parent
+                } else {
+                    $BagRulesParsedReversed.Add($PSItem,@($Parent))
+                }    
+            }  
+        }
+        
         $BagRulesParsed.Add($Parent,$Children)
     }
 
@@ -47,7 +54,7 @@ function Traverse-Tree {
 
     $parents = $haystack[$needle]
     if(-not($parents)) { return }
-    $parents | foreach {
+    $parents | ForEach-Object {
         $Bags.Add($PSItem) | Out-Null
         Traverse-Tree -needle $PSItem -haystack $haystack 
     }
@@ -57,3 +64,28 @@ Traverse-Tree -needle "shiny gold" -haystack $BG.tree_reversed
 
 Write-Output ("How many bag colors can eventually contain at least one shiny gold bag? {0}" -f $Bags.Count)
 
+#PART 2
+function Traverse-OnceAgain {
+    [CmdletBinding()]
+    param (
+        $start,
+        $where
+    )
+
+    Write-Debug -Message "I'm in $start, childnodes: $($where[$start].Keys -join '|')"
+
+    $c = $where[$start]
+    if (-not $c) {
+        return 0
+    }
+
+    foreach ($child in $c.GetEnumerator()) {
+        $add = $child.Value
+        $tothisshit = Traverse-OnceAgain -start $child.Key -where $where
+        ($add + $add * ($tothisshit | Measure-Object -Sum).Sum)
+    }
+}
+
+$Result = Traverse-OnceAgain -start "shiny gold" -where $BG.tree
+
+($Result | Measure-Object -Sum).Sum
