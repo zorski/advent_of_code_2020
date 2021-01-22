240a45f1e070cd8eb0e987b51af23ee6ad1a358a
zorski <zorski@kali02.zaor.ski>
2021-01-22 03:44:10 +0100

    day10 part 2

--- day10.ps1
+++ day10.ps1
@@ -1,5 +1,18 @@
 $Adapters = Get-Content -Path ./day10_input.txt | ForEach-Object { [int]$_ } | Sort-Object
 $Adapters = @(0) + $Adapters + @($Adapters[-1] + 3)
+# $Adapters = @"
+# 16
+# 10
+# 15
+# 5
+# 1
+# 11
+# 7
+# 19
+# 6
+# 12
+# 4
+# "@ -split "`n" | ForEach-Object { [int]$_ } | Sort-Object
 
 $Results = [PSCustomObject]@{
     One = 0
@@ -17,6 +30,44 @@ while ($i -lt $Adapters.Count - 1) {
 }
 
 "PART 1: $($Results.One * $Results.Three)"
+
+
+$Sum = @{Sum = 0}
+$Memo = @{}
+function CountDistinctAssignments {
+    param (
+        $Adapters,
+        [int]$Start,
+        $Sum
+    )
+
+    if ($Memo.ContainsKey($Start)) {
+        $Sum.Sum += $Memo[$Start]
+        return
+    }
+
+    if($Start -eq $Adapters[-1]) {
+        $Sum.Sum += 1
+        return
+    }
+
+    if ($Adapters -contains ($Start+1)) {
+        CountDistinctAssignments -Adapters $Adapters -Start ($Start+1) -sum $Sum
+    }
+
+    if ($Adapters -contains ($Start+2)) {
+        CountDistinctAssignments -Adapters $Adapters -Start ($Start+2) -sum $Sum
+    }
+
+    if ($Adapters -contains ($Start+3)) {
+        CountDistinctAssignments -Adapters $Adapters -Start ($Start+3) -sum $Sum
+    }
+
+    $Memo.Add($Start,$Sum.Sum)
+}
+
+CountDistinctAssignments -Adapters $Adapters -start $Adapters[0] -Sum $Sum
+"PART 2: $($Sum["Sum"])"
 
 
 
