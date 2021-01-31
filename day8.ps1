d4f973ce18a496f0bd8e61973f824b233e5bb181
Michal Zaorski <m.zaorski@icloud.com>
2021-01-31 10:58:52 +0100

    day8 - part2 beggining

--- day8.ps1
+++ day8.ps1
@@ -36,3 +36,52 @@ do {
 } while ($visited.Add($i))
 
 Write-Output "Accumulator = $Accumulator"
+
+# PART 2 
+
+$Visited.Clear()
+$Stack = [System.Collections.Generic.Stack[int]]::new()
+$i = 0
+$Accumulator = 0
+$Flag = $false
+
+while ($i -lt $PuzzleInput.Count) {
+  $Instruction = Parse-Instruction -Instruction $PuzzleInput[$i]
+  # if ($Flag) {
+  #   if ($Instruction['name'] -eq "jmp") {
+  #     $Instruction['name'] = 'nop'
+  #     $Flag = $false
+  #   } elseif ($Instruction['name'] -eq "nop") {
+  #     $Instruction['name'] = "jmp"
+  #     $Flag = $false
+  #   } else {
+  #     Write-Debug "nope"
+  #   }
+  # }
+
+  if (-not($visited.Add($i))) {
+    $PreviousIndex = $Stack.Pop()
+    Write-Warning "Current Index: $i | Current: $($PuzzleInput[$i])"
+    Write-Warning "Previous Index: $PreviousIndex | Previous: $($PuzzleInput[$PreviousIndex])"
+    # $Flag = $true
+    # $i = $PreviousIndex
+    # $Visited.Remove($i)
+    # continue
+    break
+  }
+  $Stack.Push($i)
+  switch ($Instruction['name']) {
+    'nop' { 
+      $i++
+    }
+    'jmp' {
+      $i += $Instruction['arg']
+    }
+    'acc' {
+      $Accumulator += $Instruction['arg'] 
+      $i++
+    }
+  }
+}
+
+$Accumulator
